94121ff11c7b1e6aa1d723cef73fc1b822ae40cf
lukas66666666 <lukas.cada007@gmail.com>
2026-09-08 16:39:30 +0200

    add design

--- src/main.js
+++ src/main.js
@@ -5,6 +5,22 @@ submitBtn.addEventListener("click", function (){
 
     fetch(`/api/search-song?q=${encodeURIComponent(input)}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            document.getElementById("results").innerHTML = ""
+            for (let i=0; i < 3; i++){
+                let songName = data.response.hits[i].result.title
+                let songArtist = data.response.hits[i].result.primary_artist.name
+                let songImage = data.response.hits[i].result.song_art_image_thumbnail_url
+
+                document.getElementById("results").innerHTML += `
+                <div class="result" id="result${i}">
+                    <img src="${songImage}" class="resultImg">
+                    <p class="resultName">${songName}</p>
+                    <p class="resultArtist">${songArtist}</p>
+                </div>
+                `
+            }
+        })
 })
 
